ae6b055aaccb49a7be6b7a0699c1f7378ed7d549
= <ariel.evangelista@outlook.com>
2019-05-14 10:12:36 -0300

    Tour of Heroes, atualizado com services

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './Hero'
+
+export const HEROES: Hero[] = [
+    {id:11, name: 'Mr. Nice'},
+    {id:12, name: 'Boo'},
+    {id:13, name: 'Shark'},
+    {id:14, name: 'Loki'},
+    {id:15, name: 'Glorious'}
+];
